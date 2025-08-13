5fbd9467d348cc484d918afc9ad4b60d44b8546f
Jamie Bryson <jamieab98@gmail.com>
2025-08-13 16:32:10 -0400

    created the functionthat accepts the arguments

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-function createLoginTracker() {
-  
+function createLoginTracker(userInfo) {
+  console.log(userInfo.username);
+  console.log(userInfo.password);
 }
+
+const jamie = {username: "jamieab98", password: "09091998"};
+createLoginTracker(jamie);
 
 
 module.exports = {
