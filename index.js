9d8a1eb38aac7bcbc8c7c3c0b45b6fe4562857d9
Jamie Bryson <jamieab98@gmail.com>
2025-08-23 10:07:46 -0400

    finished the code

--- index.js
+++ index.js
@@ -1,35 +1,21 @@
 function createLoginTracker(userInfo) {
+  const { username, password } = userInfo;
   let attemptCount = 0;
-  //console.log(attemptCount);
-  const attempt = (passwordAttempt) => {
-    //console.log("tracker return here");
-    attemptCount += 1;
-    console.log(attemptCount);
-    if (attemptCount > 3) {
-      console.log("Account locked due to too many failed login attempts");
+
+  /*return (passwordAttempt) => {
+    attemptCount++;
+
+    if (passwordAttempt === password && attemptCount <= 3) {
+      return `Login successful`;
     }
-    else {
-      if (passwordAttempt === userInfo.password) {
-        console.log("Login successful");
-      }
-      else {
-        console.log(`Attempt ${attemptCount}: Login Failed`);
-      }
+
+    if (attemptCount <= 3) {
+      return `Attempt ${attemptCount}: Login failed`;
     }
-  }
-  attempt(userInput);
-  //console.log(userInfo.username);
-  //console.log(userInfo.password);
+
+    return `Account locked due to too many failed login attempts`;
+  };*/
 }
-
-const jamieab98 = {
-  username: "jamieab98", 
-  password: "09091998"
-};
-
-let userInput = "0909198";
-createLoginTracker(jamieab98);
-
 
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
